15158d883a6496ce1e18facac8afa3c5903c9188
steve zhang <871506263@qq.com>
2021-04-11 22:07:05 +0800

    first commit

---
+++ src/components/CompA.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="comp-a" @click="$emit('sonClick', 'i am son comp')">
+    组件A的内容
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.comp-a {
+  height: 100px;
+  width: 300px;
+  margin: 20px auto;
+  background: #ccc;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    首页 组件A:
+    <CompA @sonClick="handleSonClick" @click.native.prevent="handleNativeClick"></CompA>
+  </div>
+</template>
+
+<script>
+import CompA from '@/components/CompA.vue';
+export default {
+  components: {
+    CompA
+  },
+  methods: {
+    handleSonClick(val) {
+      console.log('val :>> ', val);
+    },
+    handleNativeClick() {
+      console.log('native click by father :>> ', 1);
+    }
+  }
+};
+</script>
+
+<style></style>
